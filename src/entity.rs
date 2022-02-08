eb175711535b777832aa35d899e92782f57ead45
confessore <confessore@balasolu.com>
2022-02-08 17:23:40 -0500

    entity trait? let's use this

--- src/entity.rs
+++ src/entity.rs
@@ -16,3 +16,20 @@ pub struct Entity {
 
     pub angle: Angle
 }
+
+pub trait Entity {
+    fn id() -> String;
+    fn name() -> String;
+    fn callsign() -> String;
+    // x, y, z in relation to a reference celestial body origin
+    fn position() -> Position;
+    // the direction in which the bow of the vessel is pointed
+    fn heading() -> Heading;
+    // direction in which the vessel should be steered
+    fn course() -> Course;
+    fn bearing() -> Bearing;
+    fn track() -> Track;
+    fn route() -> Route;
+    fn orientation() -> Orientation;
+    fn angle() -> Angle;
+}
